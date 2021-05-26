cd914532d21acc3da9ab5bef2839191c55c25379
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-05-26 08:58:41 -0400

    completed printString recursion function

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Code your solution here!
+function printString(str) {
+    console.log(str[0])
+    if (str.length > 1){
+        return printString(str.substr(1, str.length))}
+    else {
+        return true
+    }
+}
 
