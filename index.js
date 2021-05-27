70eebe5db247227371849da2426558806164f279
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-05-27 11:44:23 -0400

    completed isPalindrome recursion function

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ function reverseString(str) {
     return str.substring(str.length - 1) + reverseString(str.substring(0, str.length - 1))
 }
 
+
+
+function isPalindrome(str) {
+    
+    if(str.length > 1) {
+        isPalindrome(str.substring(1, str.length - 1))
+        return str.endsWith(str[0])        
+    }else {
+        return true
+    }
+}
+
