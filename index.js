a75f965f499bf284bd22621b750933b1bf349531
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-05-27 11:12:32 -0400

    completed reverseString recursion function

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 function printString(str) {
     console.log(str[0])
     if (str.length > 1){
-        return printString(str.substr(1, str.length))}
+        return printString(str.substring(1, str.length))}
     else {
         return true
     }
+}
+
+
+function reverseString(str) {
+    
+    if(str.length ===  0){
+        return ''
+    } 
+    return str.substring(str.length - 1) + reverseString(str.substring(0, str.length - 1))
 }
 
